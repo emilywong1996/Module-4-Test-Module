4646b4ddb4362b0ecdbb4d6c2bde6fd9da516fe3
emilywong1996 <emily.wong.cohort229@gmail.com>
2023-04-17 18:40:24 -0700

    second commit

--- test/Express_Node_Introduction.js
+++ test/Express_Node_Introduction.js
@@ -92,3 +92,63 @@ server.listen(port, hostname, function () {
 // const square = require("./square");
 // console.log(`The area of a square with a width of 4 is ${square.area(4)}`);
 
+// Using asynchronous APIs
+
+// Asynchronous API is when the API will start an operation and immediately return. Once the operation finishes, the API will use some mechanism to perform additional operations.
+
+// For example: the code will print out "Second, First" because even though setTimeout() method is called first, and returns immediately, the operation doesn't complete for several seconds.
+
+// Using non-blocking asynchronous APIs is even more important on node than in the browser because Node is a single-threaded event-driven execution environment.
+
+// "Single threaded" means that all requests to the server are run on the same thread (rather than being spawned off into separate processes).
+
+// Creating route handlers
+
+// app.get("/", function (req, res) {
+//   res.send("Hello World!");
+// });
+
+// The callback function takes a request and a response object as arguments. In this case, the method calls send() on the response to return the string "Hello World!"
+
+// You could call res.json() to send a JSON response or res.sendFile() to send a file.
+
+// There is a special routing method, app.all() which will be called in response to any HTTP method.  This is used for loading middleware functions at a particular path for all request methods
+
+// The example shows a handler that will be executed for requests to /secret irrespective of the HTTP verb used
+
+// appendFile.all("/secret", function (req, res, next) {
+//   console.log("Accessing the secret selection...");
+//   next();
+// });
+
+// Routes allow you to match particular patterns of characters in a URL, and extract some values from the URL and pass them as parameters to the route handler.
+
+// Often it is useful to group route handlers for a particular part of a site together and access them using a common route-prefix.  In express this is achieved by using the express.Router object.
+
+// const express = require("express");
+// const router = express.Router();
+
+// router.get("/", function (req, res) {
+//  resizeBy.send("Wiki home page");
+//})
+
+// router. get("/about", function (req, res) {
+//   res.send("About this wiki");
+// });
+
+// module.exports = router;
+
+// Using middleware
+
+// Middleware is used for tasks from serving static files to error handling, to compressing HTTP responses.
+
+// Route functions end the HTTP request-response cycle by returning some response to the HTTP client, middleware functions typically perform some operation on the request or response and then call the function in the "stack," which might be more middleware or a route handler.
+
+// Most apps will use third-party middleware in order to simplify common web development tasks like working with cookies, sessions, user authentication, accessing request POST and JSON data, logging, etc.
+
+// The only difference between a middleware function and a route handler callback is that middleware functions have a third argument next, which middleware functions are expected to call if they are not that which completes the request cycle.
+
+// Middleware functions can be added to the processing chain for all responses with app.use(), or for a specific HTTP verb using the associated method: app.get(), app.post(), etc.
+
+//Routes are specified the same way for both cases, though the route is optional when calling app.use().
+
